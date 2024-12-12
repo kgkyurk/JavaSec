20d2e1ee7679c631225a971b8339a30d9c30003b
zhufangzheng <erosionzhu@outlook.com>
2024-12-12 23:11:57 +0800

    完善Java类转字节码工具

--- B - 反序列化/Java类转字节码工具/JavaClassByteUtil.java
+++ B - 反序列化/Java类转字节码工具/JavaClassByteUtil.java
@@ -1,16 +1,3 @@
-学Java安全的时候发现好多师傅分析漏洞的时候直接就给了一段Base64字节码，然后虽然一些师傅也会给出来对应的代码，但是有些小白是不知道怎么把一个java类转成Base64字节码的。
-
-对于刚学Java安全的小白来说真的太不友好啦，也不知道这玩意儿是什么意思，所以这里写一个小工具，专门用来把Java类转成Base64字节码，以及把字节码转成.class文件，这样即便有些师傅只给了Base64我们也可以用这份代码还原一下师傅用的字节码文件。
-
-代码中给了几个工具的示例用法。
-
-## Java代码
-
-可以看链接中的Java代码，点击这里[JavaClassByteUtil](./JavaClassByteUtil)
-
-
-
-```java
 package fastjson_labs.utils;
 
 import javax.tools.JavaCompiler;
@@ -127,7 +114,7 @@ public class JavaClassByteUtil {
         // 示例1: 将Exp.java编译后再转换成base64
         String base64Byte = JavaClassByteUtil.compile2Base64Byte("C:\\Users\\15137\\Desktop\\Workspace\\vuln-learn\\src\\test\\java\\fastjson_labs\\version23\\Exp.java");
         System.out.println(base64Byte);
-        
+
         // 示例2: 将base64编码的字符串转换成.classFile,也就是C:\Users\15137\Desktop\DecodedTest.class
         JavaClassByteUtil.base64Byte2JavaClassFile(base64Byte, "C:\\Users\\15137\\Desktop");
 
@@ -138,25 +125,3 @@ public class JavaClassByteUtil {
         JavaClassByteUtil.byte2JavaClassFile(expByte, new File("C:\\Users\\15137\\Desktop\\Exp.class"));
     }
 }
-```
-
-我要转换的Java代码如下：
-
-![image-20241209154758370](./main.assets/image-20241209154758370.png)
-
-然后执行脚本会有一些输出：
-
-```bash
-# 一些警告......
-yv66vgAAADQAIQoABgATCgAUABUIABYKABQAFwcAGAcAGQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAApFeGNlcHRpb25zBwAaAQAJdHJhbnNmb3JtAQCmKExjb20vc3VuL29yZy9hcGFjaGUveGFsYW4vaW50ZXJuYWwveHNsdGMvRE9NO0xjb20vc3VuL29yZy9hcGFjaGUveG1sL2ludGVybmFsL2R0bS9EVE1BeGlzSXRlcmF0b3I7TGNvbS9zdW4vb3JnL2FwYWNoZS94bWwvaW50ZXJuYWwvc2VyaWFsaXplci9TZXJpYWxpemF0aW9uSGFuZGxlcjspVgEAcihMY29tL3N1bi9vcmcvYXBhY2hlL3hhbGFuL2ludGVybmFsL3hzbHRjL0RPTTtbTGNvbS9zdW4vb3JnL2FwYWNoZS94bWwvaW50ZXJuYWwvc2VyaWFsaXplci9TZXJpYWxpemF0aW9uSGFuZGxlcjspVgcAGwEAClNvdXJjZUZpbGUBAAhFeHAuamF2YQwABwAIBwAcDAAdAB4BAARjYWxjDAAfACABABtmYXN0anNvbl9sYWJzL3ZlcnNpb24yMy9FeHABAEBjb20vc3VuL29yZy9hcGFjaGUveGFsYW4vaW50ZXJuYWwveHNsdGMvcnVudGltZS9BYnN0cmFjdFRyYW5zbGV0AQATamF2YS9pby9JT0V4Y2VwdGlvbgEAOWNvbS9zdW4vb3JnL2FwYWNoZS94YWxhbi9pbnRlcm5hbC94c2x0Yy9UcmFuc2xldEV4Y2VwdGlvbgEAEWphdmEvbGFuZy9SdW50aW1lAQAKZ2V0UnVudGltZQEAFSgpTGphdmEvbGFuZy9SdW50aW1lOwEABGV4ZWMBACcoTGphdmEvbGFuZy9TdHJpbmc7KUxqYXZhL2xhbmcvUHJvY2VzczsAIQAFAAYAAAAAAAMAAQAHAAgAAgAJAAAALgACAAEAAAAOKrcAAbgAAhIDtgAEV7EAAAABAAoAAAAOAAMAAAAMAAQADQANAA4ACwAAAAQAAQAMAAEADQAOAAEACQAAABkAAAAEAAAAAbEAAAABAAoAAAAGAAEAAAARAAEADQAPAAIACQAAABkAAAADAAAAAbEAAAABAAoAAAAGAAEAAAAVAAsAAAAEAAEAEAABABEAAAACABI=
-Class file decoded successfully!
-
-```
-
-最终在你给出的路径下回输出一个
-
-![image-20241209154517413](./main.assets/image-20241209154517413.png)
-
-同时也会输出一个Exp.class，当然这和DecodedTest.class区别只是一个是纯byte[]，另一个是byte[] 被base64编码之后的字符串而已
-
-![image-20241212230737635](./main.assets/image-20241212230737635.png)
